ad5b58a096da42105a6644f636952e9d03bddc8c
winnerboy7 <winnerboy7@hotmail.com>
2025-11-22 18:31:13 +0700

    Config Application

--- backend/index.js
+++ backend/index.js
@@ -24,24 +24,29 @@ app.get("/video", function (req, res) {
   if (!range) {
     res.status(400).send("Requires Range header")
   }
-  const videoFileName = req.query.fileName || "file_example_MP4_1920_18MG.mp4"
+  const videoFileName = req.query.fileName || "file_example.mp4"
   // const videoPath ="videos/" + videoFileName
-  const videoPath ="videos/" + videoFileName
+  const videoPath = "videos/" + videoFileName
 
-  const videoSize = fs.statSync(videoPath).size
-  const CHUNK_SIZE = 10 ** 6 // 1MB
-  const start = Number(range.replace(/\D/g, ""))
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-  const contentLength = end - start + 1
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
+  if (!fs.existsSync(videoPath)) {
+    console.log("File does not exist.")
+    res.status(404).send("File not founded.")
+  } else {
+    const videoSize = fs.statSync(videoPath).size
+    const CHUNK_SIZE = 10 ** 6 // 1MB
+    const start = Number(range.replace(/\D/g, ""))
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
+    const contentLength = end - start + 1
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    }
+    res.writeHead(206, headers)
+    const videoStream = fs.createReadStream(videoPath, { start, end })
+    videoStream.pipe(res)
   }
-  res.writeHead(206, headers)
-  const videoStream = fs.createReadStream(videoPath, { start, end })
-  videoStream.pipe(res)
 })
 
 const PORT = process.env.PORT || 3000
